3b9f5fc71146c24ff752ae0a80ea07b99ba6030f
maqalaqil <maheralaqil1993@gmail.com>
2020-10-20 11:44:57 +0300

    added basic config

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-black">learn react</p>
+    </div>
+  );
+}
+
+export default App;
