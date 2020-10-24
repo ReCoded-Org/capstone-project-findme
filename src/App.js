f8ccbf3a308185ff33a495fd5d54ee57a243b7ab
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-10-24 10:17:33 +0300

    Feat: add Statistics component
    add Statistics component to main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import './styles/main.css';
 
+import { Statitics } from './components/Statistics/Statistics';
+import lensIcon from './images/lens.svg';
+import glassesIcon from './images/glasses.svg';
+import locationIcon from './images/location.svg';
+
 function App() {
+  // Statistics data
+  const info = [
+    {
+      id: 1,
+      text: 'مشاهدة',
+      icon: glassesIcon,
+      figure: 1250,
+    },
+    {
+      id: 2,
+      text: 'شخص تم إيجاده',
+      icon: lensIcon,
+      figure: 16,
+    },
+    {
+      id: 3,
+      text: 'مفقود',
+      icon: locationIcon,
+      figure: 240,
+    },
+  ];
   return (
     <div className="App">
-      <p className="text-black">learn react</p>
+      <Statitics info={info} />
     </div>
   );
 }
