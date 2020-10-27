00ae81e4bfda56ccd3778b2f3af5045b300441cb
SondosAhmed <sondosalshami@gmail.com>
2020-10-27 12:36:40 +0300

    Fix: Fixing text

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './styles/main.css';
+import Slider from './containers/Home/SliderCards';
 
 function App() {
   return (
     <div className="App">
-      <p className="text-black"> learn react </p>{' '}
+      <p className="text-black">learn react</p>
+      <Slider />
     </div>
   );
 }
