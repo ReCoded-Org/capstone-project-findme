2a2d7bc059ddfc783ac208aae3589b45a9b70ef2
AhmedAl-Majidi <67476296+AhmedAl-Majidi@users.noreply.github.com>
2020-10-27 12:45:21 +0300

    style: adjust font colors

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
   return (
     <div>
       <button
-        className="rounded-full w-40 h-12 bg-gradient-to-r from-blue-200 to-blue-500"
+        className="rounded-full w-40 h-12 bg-gradient-to-r from-blue-400 to-blue-700 text-white"
         onClick={props.onClickMethod}
       >
         <div className="flex flex-row-reverse justify-center">
