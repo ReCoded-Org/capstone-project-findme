611ccbe8fa90d5eb5f7e8513138c67867ca42b52
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-10-31 23:37:10 +0300

    Fix: make all dynamic content comes from mock-up api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './styles/main.css';
+// import MissingPeople from './containers/HomePage/MissingPeople';
 import MissingPeople from './containers/HomePage/MissingPeople';
+import MissingPeoplePage from './containers/MissingPeople/MissingPeoplePage';
 
 function App() {
   return (
     <div className="App">
       <MissingPeople missingPeople="Missing People" />
+
+      <hr />
+      {/* <MissingPeoplePage /> */}
     </div>
   );
 }
--- src/components/MissingPersonCard.jsx
+++ src/components/MissingPersonCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import userImage from '../images/profile-svg.svg';
 
-const MissingPersonCard = ({ userName, lostSince }) => {
+const MissingPersonCard = ({ cardInfo, i }) => {
   const [footerCard, setfooterCard] = useState(0);
 
   const beforeHover = (
@@ -13,19 +13,19 @@ const MissingPersonCard = ({ userName, lostSince }) => {
       <div className="shadow-lg rounded-3xl max-w-xs overflow sm:self-center sm:mx-auto">
         <div className="relative">
           <span className="absolute inline-block pr-4 pl-6 py-2 font-bold md:pr-2 md:pl-3 md:py-1 lg:pl-6 lg-pr-4 lg:py-2 bg-blue-700 right-0 text-white rounded-l-3xl mt-8 md:mt-4 lg:mt-8">
-            Found
+            {cardInfo.lostFamily ? 'Found' : 'Lost'}
           </span>
 
           <img
             className="opacity-50 cursor-pointer"
-            src={userImage}
-            alt="username"
+            src={cardInfo.img}
+            alt={`${cardInfo.userName} image`}
             width={300}
           />
         </div>
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{userName}</div>
-          <p className="text-base font-light">{`Lost sice: ${lostSince}`}</p>
+          <div className="font-bold text-xl mb-2">{cardInfo.userName}</div>
+          <p className="text-base font-light">{`Lost sice: ${cardInfo.lostSince}`}</p>
         </div>
       </div>
     </div>
@@ -39,20 +39,19 @@ const MissingPersonCard = ({ userName, lostSince }) => {
       <div className="shadow-lg rounded-3xl max-w-xs overflow sm:self-center sm:mx-auto">
         <div className="relative">
           <span className="absolute inline-block pr-4 pl-6 py-2 font-bold md:pr-2 md:pl-3 md:py-1 lg-pr-4 lg:pl-6 lg:py-2 bg-blue-700 right-0 text-white rounded-l-3xl mt-8 md:mt-4 lg:mt-8">
-            Found
+            {cardInfo.lostFamily ? 'Found' : 'Lost'}
           </span>
           <p className="absolute bottom-0 mb-20 mx-10 md:m-auto md:text-xs md:mx-2 md:mb-8 md:leading-3 lg:mx-10 lg:text-base lg:mb-20 lg:leading-tight	">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
-            reiciendis aut, laudantium dolorem itaque aliquid.
+            {cardInfo.description}
           </p>
           <div className="absolute bottom-0 ml-8 mb-2 md:text-xs md:ml-2 md:mb-1 lg:text-base lg:ml-8 lg:mb-2">
-            <span className="mr-8 md:mr-2 lg:mr-8">Age: 17</span>
+            <span className="mr-8 md:mr-2 lg:mr-8">Age: {cardInfo.age}</span>
             <span>Location: Sana'a</span>
           </div>
           <img
             className="opacity-25 cursor-pointer"
-            src={userImage}
-            alt="username"
+            src={cardInfo.img}
+            alt={`${cardInfo.userName} image`}
             width={300}
           />
         </div>
@@ -60,7 +59,9 @@ const MissingPersonCard = ({ userName, lostSince }) => {
           {/*<div className="font-bold text-xl mb-2">{userName}</div>*/}
           <div className="flex justify-between md:flex-col lg:flex-row xl:flex-row bg-blue-700 ">
             <div className="items-center mt-2">
-              <p className="md:mb-2 mx-4 font-semibold">129 veiws</p>
+              <p className="md:mb-2 mx-4 font-semibold">
+                {cardInfo.views} views
+              </p>
             </div>
             <div className=" ">
               <a href="#">
