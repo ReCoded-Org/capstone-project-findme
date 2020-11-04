78a4db8d61d22f34ba0cc5218e8c29e4d8c0dc4d
BasilHomadi <deviltearbasil@gmail.com>
2020-11-04 12:01:16 +0300

    Fix: Navbar in not loading probably

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Navbar from './components/NavBar/NavBar';
+import Navbar from './components/navbar/NavBar';
 
 ReactDOM.render(
   <React.StrictMode>
