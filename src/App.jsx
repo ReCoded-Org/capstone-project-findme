66980bf0124f10596931c3e6dd884aa1c4ed90f6
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-11-07 20:18:12 +0300

    combine navbar and Slider cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles/main.css';
-import ContacUs from './containers/ContactUs/ContactUsContainer';
+import Navbar from './components/navbar/NavBar';
+import SliderCards from './containers/Home/SliderCards';
 
 function App() {
   return (
     <div className="App bg-findMe">
-      <p className="text-black">learn react</p>
-      <ContacUs />
+      <Navbar />
+      <hr />
+      <SliderCards />
     </div>
   );
 }
