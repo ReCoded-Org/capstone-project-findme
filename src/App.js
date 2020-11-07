0b5fb07a729b7caff083a84fc10213c84d8f5084
AhmedAl-Majidi <67476296+AhmedAl-Majidi@users.noreply.github.com>
2020-11-07 12:39:58 +0300

    fix: refactor the code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import React from 'react';
 import './styles/main.css';
-
-import { Statitics } from './components/Statistics/Statistics';
-import lensIcon from './images/lens.svg';
-import glassesIcon from './images/glasses.svg';
-import locationIcon from './images/location.svg';
+import WebsiteDescription from './container/WebsiteDescription/WebsiteDescription';
 
 function App() {
-  // Statistics data
-  const info = [
-    {
-      id: 1,
-      text: 'مشاهدة',
-      icon: glassesIcon,
-      figure: 1250,
-    },
-    {
-      id: 2,
-      text: 'شخص تم إيجاده',
-      icon: lensIcon,
-      figure: 16,
-    },
-    {
-      id: 3,
-      text: 'مفقود',
-      icon: locationIcon,
-      figure: 240,
-    },
-  ];
   return (
     <div className="App">
-      <Statitics info={info} />
+      <WebsiteDescription /> 
     </div>
   );
 }
