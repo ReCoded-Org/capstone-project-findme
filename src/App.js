a6152d2dd5ee438124f0334a5514557a680d9963
maqalaqil <maheralaqil1993@gmail.com>
2020-11-09 12:07:54 +0300

    fixed test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles/main.css';
 // import footer section
 import Footer from './containers/Home/FooterSection';
-import MissingPersonForm from './components/MissingPersonForm/MissingPersonForm';
+
 // import RouterPages from './router/RouterPages';
 function App() {
   return (
