576cbc88d011242ec3f93740b18964cb7775f487
as-kolaib <as-kolaib@github.com>
2020-11-08 16:55:48 -0800

    Feat:Adding Language Settings

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
-//import i18next-browser-languageDetector For Auto detect the user language
-import detector from "i18next-browser-languagedetector";
-import { initReactI18next } from 'react-i18next';
+// import i18next-browser-languageDetector For Auto detect the user language
+import detector from 'i18next-browser-languagedetector';
+import { reactI18nextModule } from 'react-i18next';
 
 // the translations
 // (tip move them in a JSON file and import them)
@@ -18,20 +18,19 @@ const resources = {
   },
 };
 
-
 i18n
   .use(detector)
   .use(reactI18nextModule) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "en",
-    fallbackLng: "en", // use en if detected lng is not available
+    lng: 'en',
+    fallbackLng: 'en', // use en if detected lng is not available
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
     interpolation: {
-      escapeValue: false // react already safes from xss
-    }
+      escapeValue: false, // react already safes from xss
+    },
   });
 
 export default i18n;
