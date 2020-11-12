003f4bba8baf124f79b50735aefadccc2eb8afc9
SondosAhmed <sondosalshami@gmail.com>
2020-11-12 10:36:16 +0300

    Feat: Creating Post page

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import './styles/main.css';
-import Footer from './containers/Home/FooterSection';
-import Navbar from './components/navbar/NavBar';
+import '../styles/main.css';
+import Footer from '../containers/Home/FooterSection';
+import Navbar from '../components/navbar/NavBar';
+import Add from '../containers/AddMissingPesrson/AddMissingPerson'
 
-import Post from './containers/AddMissingPesrson/AddMissingPerson'
 
 function Post() {
   return (
     <div className="App bg-findMe">
       <Navbar />
       <hr />
-      <Post />
+      <Add />
       <Footer />
     </div>
   );
