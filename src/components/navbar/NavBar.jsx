0011654749cc3b80c3f3f1f1ee942b5c472ed028
BasilHomadi <deviltearbasil@gmail.com>
2020-11-14 17:07:26 +0300

    Fix: Align ul to match the logo text

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -76,7 +76,7 @@ const Navbar = (props) => {
           navbarOpen ? 'flex' : 'hidden'
         } xl:flex flex-grow items-center`}
       >
-        <ul className="flex flex-col xl:flex-row list-none xl:ml-auto items-start xl:items-center">
+        <ul className="ml-10 xl:ml-0 flex flex-col xl:flex-row list-none xl:ml-auto items-start xl:items-center">
           <>
             <li className="mt-2 xl:mt-0">
               <Link
