467745452f6bba09c8db190bdddb93ae4b3051a4
maqalaqil <maheralaqil1993@gmail.com>
2020-11-17 01:18:29 +0300

    revert

--- src/components/MissedPersonOtherData/MissedPersonOtherData.jsx
+++ src/components/MissedPersonOtherData/MissedPersonOtherData.jsx
@@ -3,28 +3,32 @@ import React from 'react';
 import RowOfData from '../RowOfData/RowOfData';
 import Data from '../../Data';
 
+import {useTranslation} from "react-i18next";
+
 const MissedPersonOtherData = () => {
+  const [t, i18n] = useTranslation('common');
+
   return (
     <div className="lg:w-2/4 ml-4 lg:ml-0">
       <div className="mb-4">
-        <p className="text-gray-500">Informations</p>
+        <p className="text-gray-500">{t('translation.informations')}</p>
         <hr className="x" />
       </div>
       {/* Informations Section */}
       <div className="flex flex-col text-sm sm:flex-row lg:text-md">
         <div className="flex flex-col justify-center w-full md:w-2/4">
-          <RowOfData label="Location of lose:" data={Data[1].locationOfLose} />
-          <RowOfData label="Lost since:" data={Data[1].lostSince} />
-          <RowOfData label="Age:" data={Data[1].age} />
-          <RowOfData label="Notes:" data={Data[1].notes} />
+          <RowOfData label={t('translation.locationOfLossD')} data={Data[1].locationOfLose} />
+          <RowOfData label={t('translation.lostSinceD')} data={Data[1].lostSince} />
+          <RowOfData label={t('translation.ageD')} data={Data[1].age} />
+          <RowOfData label={t('translation.notesD')} data={Data[1].notes} />
         </div>
 
         <div className="flex flex-col justify-center w-full md:w-2/4">
-          <RowOfData label="Last place seen:" data={Data[1].lastPlaceSeen} />
-          <RowOfData label="Last update:" data={Data[1].lastUpdate} />
-          <RowOfData label="Gender:" data={Data[1].gender} />
+          <RowOfData label={t('translation.lastPlaceSeenD')} data={Data[1].lastPlaceSeen} />
+          <RowOfData label={t('translation.lastUpdateD')} data={Data[1].lastUpdate} />
+          <RowOfData label={t('translation.genderD')} data={Data[1].gender} />
           <RowOfData
-            label="Special situation:"
+            label={t('translation.specialSituationD')}
             data={Data[1].specialSituation}
           />
         </div>
@@ -32,17 +36,17 @@ const MissedPersonOtherData = () => {
 
       {/* Poster and Contact Info Section */}
       <div className="my-4">
-        <p className="text-gray-500">Poster and Contact Info</p>
+        <p className="text-gray-500">{t('translation.posterAndContactInfo')}</p>
         <hr className="x" />
       </div>
       <div className="flex flex-col text-sm sm:flex-row lg:text-md mb-8 sm:mb-20">
         <div className="flex flex-col justify-center w-full md:w-2/4">
-          <RowOfData label="Name:" data={Data[1].contactName} />
-          <RowOfData label="Phone number:" data={Data[1].phoneNumber} />
+          <RowOfData label={t('translation.nameD')} data={Data[1].contactName} />
+          <RowOfData label={t('translation.phoneNumberD')} data={Data[1].phoneNumber} />
         </div>
         <div className="flex flex-col justify-center w-full md:w-2/4">
-          <RowOfData label="Realtionship:" data={Data[1].realtionship} />
-          <RowOfData label="Phone number 2:" data={Data[1].phoneNumber2} />
+          <RowOfData label={t('translation.relationshipD')} data={Data[1].realtionship} />
+          <RowOfData label={t('translation.phoneNumber2D')} data={Data[1].phoneNumber2} />
         </div>
       </div>
     </div>
